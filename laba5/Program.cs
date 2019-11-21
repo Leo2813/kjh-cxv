a095d30e491f6137fe68d3bdb3e7792ee1dcf121
Leo2813 <byryshk@gmail.com>
2019-11-21 08:35:01 +0200

    Створено найпростіший консольний додаток

---
+++ laba5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace laba5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
